2f12906cbfd488c6528cd4289fdb22442c385348
João Agostinho <63238478+KrOOb-5@users.noreply.github.com>
2023-06-23 19:41:29 +0100

    ...

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 function Todo(props) {
-  //console.log(props); Nao entendo o span (html) -- So far, so good?
+  //console.log(props); 
   return (
     <li className="todo stack-small">
       <div className="c-cb">
         <input id={props.id} type="checkbox" defaultChecked={props.completed} />
-        <label className="todo-label" htmlFor={props.id}>
+        <label className="todo-label" htmlFor={props.id}> 
           {props.name} 
         </label>
       </div>
       <div className="btn-group">
         <button type="button" className="btn">
-          Edit <span className="visually-hidden">{props.name}</span>
+          Edit <span className="visually-hidden">{props.name}</span> 
         </button>
         <button type="button" className="btn btn__danger">
           Delete <span className="visually-hidden">{props.name}</span>
@@ -21,5 +21,6 @@ function Todo(props) {
     </li>
   );
 }
-
+//Span if setting visually-visible relates the button to the activity "delete {props.name}" this was more to help screen-readers
+//htmlFor is for when you submit dai ter um id especifico
 export default Todo;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const DATA = [
+    { id: "todo-0", name: "Eat", completed: true},
+    { id: "todo-1", name: "Sleep", completed: false},
+    { id: "todo-2", name: "Repeat", completed: false},
+]
+
 
 const root = ReactDOM.createRoot(document.getElementById('root')); //use strict mode to find common bugs in our components
-root.render(<App subject="KrOOb" />);
+root.render(
+    <React.StrictMode>
+        <App tasks={DATA} /> 
 
-
+    </React.StrictMode>
+);
+// pass DATA to <App /> as a prop
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
